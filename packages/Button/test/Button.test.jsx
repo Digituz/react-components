b0cd94d50a340d85b358ff7a42821361ddabafbf
Bruno Krebs <bruno.krebs@auth0.com>
2018-02-07 12:02:31 -0200

    just testing to see how it breaks;

--- packages/Button/test/Button.test.jsx
+++ packages/Button/test/Button.test.jsx
@@ -6,7 +6,7 @@ import Button from '../src/Button';
 
 describe('A suite', function() {
   it('should render without throwing an error', function() {
-    const wrapper = mount(<Button onClick={() => {}} text={'Hello, world!'} />);
+    const wrapper = mount(<Button onClick={() => {}} text={'Hello, world!!'} />);
     expect(wrapper.text()).toEqual('Hello, world!');
   });
 });
