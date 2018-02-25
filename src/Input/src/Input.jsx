f9dc1b0795dc2aea2f2e4e777c9ddc1f42df05b5
Bruno Krebs <bruno.krebs@auth0.com>
2018-02-25 15:23:39 -0300

    adding id to Input;

--- src/Input/src/Input.jsx
+++ src/Input/src/Input.jsx
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 
 class Input extends Component {
   render() {
-    const className = this.props.className || 'digituz-react-input-test';
+    const className = this.props.className || 'digituz-react-input';
     return (
       <input
+        id={this.props.id}
         className={className}
         value={this.props.value}
         onChange={this.props.onChange}
@@ -16,13 +17,15 @@ class Input extends Component {
 }
 
 Input.propTypes = {
-  className: PropTypes.string,
-  onChange: PropTypes.func.isRequired,
-  placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  id: PropTypes.string,
+  className: PropTypes.string,
+  placeholder: PropTypes.string,
 };
 
 Input.defaultProps = {
+  id: '',
   className: '',
   placeholder: '',
 };
--- src/Input/test/Input.test.jsx
+++ src/Input/test/Input.test.jsx
@@ -7,7 +7,7 @@ import Input from '../src/Input';
 describe('A suite', function() {
   it('should render without throwing an error', function() {
     const inputValue = 'Some value!';
-    const defaultClass = 'digituz-react-input-test';
+    const defaultClass = 'digituz-react-input';
 
     const wrapper = mount(<Input onChange={() => {}} value={inputValue}/>);
     const input = wrapper.find('input');
@@ -15,9 +15,18 @@ describe('A suite', function() {
     expect(wrapper.find('input').hasClass(defaultClass)).toEqual(true);
   });
 
+  it('should enable devs to set id', function() {
+    const inputValue = 'Some other value!';
+    const someId = 'random-id';
+
+    const wrapper = mount(<Input onChange={() => {}} id={someId} value={inputValue} />);
+    const input = wrapper.find(`input#${someId}`);
+    expect(input.get(0).props.value).toEqual(inputValue);
+  });
+
   it('should enable devs to change className', function() {
     const inputValue = 'Some other value!';
-    const someClass = 'digituz-react-input';
+    const someClass = 'some-class-4-input';
 
     const wrapper = mount(<Input onChange={() => {}} className={someClass} value={inputValue}/>);
     const input = wrapper.find('input');
