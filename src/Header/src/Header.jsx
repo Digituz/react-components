2541a7a8d5806ed74202c410e297244570b82c95
Bruno Krebs <bruno.krebs@auth0.com>
2018-02-25 07:41:25 -0300

    playing with CSS;

--- src/Header/src/Header.jsx
+++ src/Header/src/Header.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './Header.css';
 
 class Header extends Component {
   render() {
     const className = this.props.className || 'digituz-header';
     return (
-      <div>
-        <h1>{this.props.title}</h1>
+      <div className={className}>
+        <div>
+          <h1>{this.props.title}</h1>
+        </div>
+        <div>
+          {this.props.children}
+        </div>
       </div>
     );
   }
