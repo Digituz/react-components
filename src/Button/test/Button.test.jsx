302973e9bf0083beb1da8c6c7608e4c81ca0117c
Bruno Krebs <bruno.krebs@auth0.com>
2018-02-26 20:07:25 -0300

    fixing Button tests;

--- src/Button/test/Button.test.jsx
+++ src/Button/test/Button.test.jsx
@@ -7,11 +7,13 @@ import Button from '../src/Button';
 describe('A suite', function() {
   it('should render without throwing an error', function() {
     const buttonLabel = 'Hello, world!';
-    const defaultClass = 'digituz-react-button-test';
+    const defaultClass = 'digituz-react-button';
 
     const wrapper = mount(<Button onClick={() => {}} text={buttonLabel} />);
     expect(wrapper.text()).toEqual(buttonLabel);
     expect(wrapper.find('button').hasClass(defaultClass)).toEqual(true);
+    expect(wrapper.find('button')).toBeDefined();
+    expect(wrapper.find(`button.${defaultClass}`)).toBeDefined();
   });
 
   it('should enable devs to change className', function() {
