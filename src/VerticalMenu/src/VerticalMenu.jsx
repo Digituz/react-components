57296ac9b639c2bcad76dfbe1614f5cf1deb0bfe
Bruno Krebs <bruno.krebs@auth0.com>
2018-03-08 20:49:27 -0300

    fixing create-component.js script; enhancing VerticalMenu feature;

--- src/VerticalMenu/src/VerticalMenu.jsx
+++ src/VerticalMenu/src/VerticalMenu.jsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import 'VerticalMenu.css';
+import './VerticalMenu.css';
 
 class VerticalMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: true,
+    };
+  }
+
+  closeMenu() {
+    this.setState({
+      visible: false,
+    });
+  }
+
+  openMenu() {
+    this.setState({
+      visible: true,
+    });
+  }
+
   render() {
-    const classes = `digituz-react-vertical-menu ${this.props.className}`;
+    let classes = `digituz-react-vertical-menu ${this.props.className}`;
+    if (this.state.visible) classes += ' state-visible';
+
     return (
-      <div className={classes}></div>
+      <div onClick={() => { this.closeMenu() }} className="digituz-react-vertical-menu-overlay">
+        <div className={classes}>
+          { this.props.children }
+        </div>
+      </div>
     );
   }
 }
