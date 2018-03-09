2a3f0e1c3e2aa619aa9f2c75012835b37cf3916d
Bruno Krebs <bruno.krebs@auth0.com>
2018-03-09 13:08:08 -0300

    enhancing VerticalMenu;

--- src/VerticalMenu/src/VerticalMenu.jsx
+++ src/VerticalMenu/src/VerticalMenu.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import If from '@digituz/react-if';
 import './VerticalMenu.css';
 
 class VerticalMenu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      visible: true,
+      visible: false,
     };
   }
 
@@ -27,9 +28,27 @@ class VerticalMenu extends Component {
     if (this.state.visible) classes += ' state-visible';
 
     return (
-      <div onClick={() => { this.closeMenu() }} className="digituz-react-vertical-menu-overlay">
+      <div className="digituz-react-vertical-menu-container">
+        <div onClick={() => { this.openMenu() }}>
+          <svg className="menu-svg-button">
+            <line x1="10" y1="12" x2="30" y2="12" stroke="gray" strokeWidth="2" />
+            <line x1="10" y1="20" x2="30" y2="20" stroke="gray" strokeWidth="2" />
+            <line x1="10" y1="28" x2="30" y2="28" stroke="gray" strokeWidth="2" />
+          </svg>
+        </div>
+        <If condition={this.state.visible}>
+          <div onClick={() => { this.closeMenu() }} className="digituz-react-vertical-menu-overlay" />
+        </If>
         <div className={classes}>
-          { this.props.children }
+          <div className="menu-options-container">
+            <svg onClick={() => { this.closeMenu() }} className="menu-svg-button close-menu">
+              <line x1="11" y1="20" x2="20" y2="12" stroke="gray" strokeWidth="2" />
+              <line x1="10" y1="20" x2="30" y2="20" stroke="gray" strokeWidth="2" />
+              <line x1="11" y1="20" x2="20" y2="28" stroke="gray" strokeWidth="2" />
+            </svg>
+            { this.props.children }
+          </div>
+          <p className="powered-by-digituz">Powered by Digituz</p>
         </div>
       </div>
     );
