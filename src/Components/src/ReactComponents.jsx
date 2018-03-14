fa0329f48ef03ba98e8f759627c8a4b2efce7876
Bruno Krebs <bruno.krebs@auth0.com>
2018-03-14 19:32:02 -0300

    fixing react components

---
+++ src/Components/src/ReactComponents.jsx
@@ -0,0 +1,25 @@
+import Button from '@digituz/react-button';
+import Card from '@digituz/react-card';
+import Grid from '@digituz/react-grid';
+import If from '@digituz/react-if';
+import Input from '@digituz/react-input';
+import InputLabel from '@digituz/react-input-label';
+import Modal from '@digituz/react-modal';
+import Panel from '@digituz/react-panel';
+import PanelBody from '@digituz/react-panel-body';
+import PanelHeader from '@digituz/react-header';
+import VerticalMenu from '@digituz/react-vertical-menu';
+
+export {
+  Button,
+  Card,
+  Grid,
+  If,
+  Input,
+  InputLabel,
+  Modal,
+  Panel,
+  PanelBody,
+  PanelHeader,
+  VerticalMenu,
+}
