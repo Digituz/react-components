de6e0e800779b65301e1bd088dfc04a29f5e9540
Bruno Krebs <bruno.krebs@auth0.com>
2018-03-15 14:09:14 -0300

    debouncing prepublish script execution;

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,4 +1,4 @@
-const watch = require('scripts/watch');
+const watch = require('watch');
 const { spawnSync } = require('child_process');
 
 const watchOptions = {
@@ -6,10 +6,20 @@ const watchOptions = {
   interval: 0.5,
 };
 
-watch.createMonitor('./', watchOptions, (monitor) => {
-  monitor.on("changed", (file) => {
+let lastPrepublishTriggered = (new Date()).getTime();
+
+watch.createMonitor(`${__dirname}/../src/`, watchOptions, (monitor) => {
+  monitor.on('changed', (file) => {
     if (file.indexOf('.test.js') > -1) return;
-    if (file.indexOf('.jsx') < 0 && file.indexOf('.css') < 0) return;
+    if (file.indexOf('.jsx') < 0 && file.indexOf('.scss') < 0) return;
+
+    const now = (new Date()).getTime();
+
+    // just debouncing to avoid multiple, useless runs of  prepublishOnly
+    if (now - lastPrepublishTriggered < 2000) return;
+
+    lastPrepublishTriggered = now;
+    console.log(file);
 
     const execution = spawnSync('npm', ['run', 'prepublishOnly']);
 
